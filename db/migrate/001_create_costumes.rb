0e597ec5ae9e0936dbcc0556a7beb3b9567d88a2
travisprol <travisjohnprol@gmail.com>
2020-06-11 16:41:44 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,17 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
+end
+
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
